068ee10b896028f5885f792bd1af36d3c98db1f6
2and1or3 <2and1or3@gmail.com>
2020-09-07 16:09:42 +0300

    module8

--- gulpfile.js
+++ gulpfile.js
@@ -1,35 +1,90 @@
 "use strict";
 
-const gulp = require("gulp");
+const {task, src, dest, watch, series} = require("gulp");
 const plumber = require("gulp-plumber");
 const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
 const server = require("browser-sync").create();
+const csso = require("gulp-csso");
+const rename = require("gulp-rename");
+const del = require("del");
+const svgstore = require("gulp-svgstore");
+const imagemin = require("gulp-imagemin");
 
-gulp.task("css", function() {
-  return gulp.src("source/sass/style.scss")
+task("clean", function() {
+  return del("build");
+});
+
+task("html", function () {
+  return src("source/*.html")
+    .pipe(dest("build/"));
+});
+
+task("css", function () {
+  return src("source/sass/style.scss")
   .pipe(plumber())
   .pipe(sourcemap.init())
   .pipe(sass())
   .pipe(postcss([autoprefixer()]))
+  .pipe(csso())
+  .pipe(rename("style.min.css"))
   .pipe(sourcemap.write("."))
-  .pipe(gulp.dest("source/css"))
+  .pipe(dest("build/css"))
   .pipe(server.stream());
 });
 
-gulp.task("server", function () {
+task("js", function () {
+  return src("source/js/*.js")
+    .pipe(dest("build/js"));
+});
+
+task("font", function () {
+  return src("source/fonts/*.{woff,woff2}")
+    .pipe(dest("build/fonts"));
+});
+
+task("images", function () {
+  return src("source/img/*.{jpg,png,webp,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({quality: 80, progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo(),
+    ]))
+    .pipe(dest("build/img"));
+});
+
+task("sprite", function () {
+  return src("source/img/*.svg")
+    .pipe(svgstore({
+      inlineSvg: true,
+    }))
+    .pipe(rename("sprite.svg"))
+    .pipe(dest("build/img"));
+});
+
+task("server", function () {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
     ui: false
   });
 
-  gulp.watch("source/sass/**/*.{scss,sass}", gulp.series("css"));
-  gulp.watch("source/*.html").on("change", server.reload);
+  watch("source/sass/**/*.{scss,sass}", series("css")).on("change", server.reload);
+  watch("source/*.html", series("html")).on("change", server.reload);
 });
 
-gulp.task("start", gulp.series("css", "server"));
+task("build", series(
+    "clean",
+    "html",
+    "css",
+    "js",
+    "font",
+    "images",
+    "sprite"
+  ));
+
+task("start", series("build", "server"));
